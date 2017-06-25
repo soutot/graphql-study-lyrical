365b130b84df8af2791a4ac817c1ae41165fb93a
Tiago Souto <soutot@users.noreply.github.com>
2017-06-25 17:14:00 -0300

    Linking list to detail

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -14,8 +14,8 @@ class SongList extends Component {
     return this.props.data.songs.map(({ id, title }) => {
       return (
         <li key={id} className="collection-item">
-          {title}
-          <i 
+          <Link to={`/songs/${id}`}>{title}</Link>
+          <i
             className="material-icons"
             onClick={() => this.onSongDelete(id)}
           >
